e0cd5a0a83b48483b07f86e13e3716df911afb22
Jason Poindexter <poindexter.json@gmail.com>
2020-08-10 12:51:34 -0700

    Move ble functions to ble reducer for now

--- store/ble/reducer.ts
+++ store/ble/reducer.ts
@@ -1,5 +1,6 @@
 import {State, Device, BleError} from 'react-native-ble-plx';
 import {Action} from "./types";
+import {Buffer} from "buffer";
 
 
 export type ReduxState = {
@@ -28,6 +29,17 @@ export const initialState: ReduxState = {
     logs: [],
 };
 
+const MAX_MTU_SIZE = 128
+
+const stringToBase64 = (str: string): string => {
+    const buff = Buffer.from(str, 'utf8');
+    return buff.toString('base64');
+}
+
+const base64ToString = (base64str: string): string => {
+    const buff = Buffer.from(base64str, 'base64');
+    return buff.toString('utf8');
+}
 
 export const ble = (
     state: ReduxState = initialState,
