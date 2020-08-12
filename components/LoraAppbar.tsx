f1faff520342aad106f37cb438e2abb74b8f568f
Jason Poindexter <poindexter.json@gmail.com>
2020-08-12 13:15:14 -0700

    Underline current nav action

--- components/LoraAppbar.tsx
+++ components/LoraAppbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Appbar} from "react-native-paper";
+import {Appbar, useTheme} from "react-native-paper";
 import {StyleSheet, Text} from "react-native";
 import Chat from "../views/Chat";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {seNavigationState} from "../store/navigation/actions";
 import Settings from "../views/Settings";
 import Status from "../views/Status";
+import {RootState} from "../store";
 
 export const navigationItems: { icon: string, name: string, view: JSX.Element}[]= [
     {
@@ -24,13 +25,25 @@ export const navigationItems: { icon: string, name: string, view: JSX.Element}[]
         view: <Settings />
     }
 ]
-
 export default function LoraAppbar() {
     const dispatch = useDispatch()
+    const navigationState = useSelector(((state: RootState) => state.navigationState))
+    const { colors } = useTheme()
+    // Underline current navigation Action
+    const actionStyle = (index: number): {} => {
+        const selectedStyle =  index === navigationState && {borderBottomWidth: 5,
+            borderStyle: "solid",
+            borderBottomColor:  colors.disabled}
+        return {
+            ...styles.action,
+            ...selectedStyle
+        }
+    }
+
     const actions = navigationItems.map((navigationItem, index) => {
-        return <Appbar.Action key={navigationItem.name} style={styles.action} icon={navigationItem.icon} onPress={ () => dispatch(seNavigationState(index))} />
+        return <Appbar.Action key={navigationItem.name} style={actionStyle(index)} icon={navigationItem.icon} onPress={ () => dispatch(seNavigationState(index))} />
     })
-    return(<Appbar.Header style={{ display: "flex", justifyContent: "space-evenly"}}>
+    return(<Appbar.Header style={styles.header}>
         { actions }
     </Appbar.Header>)
 }
@@ -46,5 +59,5 @@ const styles = StyleSheet.create({
         flex: 1,
         margin: 0,
         height: "100%"
-    }
+    },
 })
